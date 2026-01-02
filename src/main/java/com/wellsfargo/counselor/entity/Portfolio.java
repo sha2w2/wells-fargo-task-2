358f6381d46746f80e69d729686253ce45fcfcec
sha2w2 <shanicesagonda@gmail.com>
2026-01-02 02:52:25 +0200

    Complete data model with Client, Portfolio and Security entities

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,48 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "clientId", nullable = false, unique = true)
+    private Client client;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date creationDate;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(Client client, Date creationDate) {
+        this.client = client;
+        this.creationDate = creationDate;
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
